7b86dbe536a6b23dd4385ab2e8c4d2ff0d332537
DurmushKartci <durmusikartci@gmail.com>
2021-08-01 22:28:52 +0300

    CSS ana renk variable eklendi , Emin Kartci Pp eklendi

--- Web/js/portfolyo.js
+++ Web/js/portfolyo.js
@@ -15,10 +15,10 @@ function add_project(category, categoryName, projectLink, imgSrc, projectTitle) 
     document.getElementById("portfolio_grid").innerHTML += newProject;
 }
 
-add_project("media", "Medya", "./portfolyo/DoralpWebsite", "./images/portfolio/DoralpWebsite.png", "Doralp Website")
-add_project("media", "Medya", "./portfolyo/MathCountingExercise", "./images/portfolio/MathProblem.png", "Math Counting Exercise")
-add_project("media", "Medya", "https://www.nazarmuzik.com/", "./images/portfolio/NazarMuzik.png", "Nazar Müzik Website")
-add_project("media", "Medya", "https://www.shefly.com/", "./images/portfolio/SheFly.png", "SheFly Website")
-add_project("media", "Medya", "https://www.mehmetkilvan.com/", "./images/portfolio/MehmetKilvan.png", "Mehmet Kilvan Website")
-add_project("media", "Medya", "https://traform-platofrmu.herokuapp.com/", "", "Traform - Trafolarda Verimlilik Projesi")
-add_project("media", "Medya", "https://evfuture.ameandanismanlik.com", "", "EV Future - Elektrikli Araç Analizi")
+add_project("website", "Medya", "./portfolyo/DoralpWebsite", "./images/portfolio/DoralpWebsite.png", "Doralp Website")
+add_project("education", "Medya", "./portfolyo/MathCountingExercise", "./images/portfolio/MathProblem.png", "Math Counting Exercise")
+add_project("website", "Medya", "https://www.nazarmuzik.com/", "./images/portfolio/NazarMuzik.png", "Nazar Müzik Website")
+add_project("website", "Medya", "https://www.shefly.com/", "./images/portfolio/SheFly.png", "SheFly Website")
+add_project("website", "Medya", "https://www.mehmetkilvan.com/", "./images/portfolio/MehmetKilvan.png", "Mehmet Kilvan Website")
+add_project("website", "Medya", "https://traform-platofrmu.herokuapp.com/", "./images/portfolio/Traform.png", "Traform - Trafolarda Verimlilik Projesi")
+add_project("project", "Medya", "https://evfuture.ameandanismanlik.com", "./images/portfolio/EVFuture.png", "EV Future - Elektrikli Araç Analizi")
